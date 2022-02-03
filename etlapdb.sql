8593898a33402e98357846e32f410753f3cef947
Poós Dániel <poosdani@gmail.com>
2022-02-03 19:15:25 +0100

    updated database

--- etlapdb.sql
+++ etlapdb.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Jan 18. 13:22
--- Kiszolgáló verziója: 10.4.21-MariaDB
--- PHP verzió: 7.4.25
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2022 at 12:05 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,15 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `etlapdb`
+-- Database: `etlapdb`
 --
-CREATE DATABASE IF NOT EXISTS `etlapdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
-USE `etlapdb`;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `etlap`
+-- Table structure for table `etlap`
 --
 
 CREATE TABLE `etlap` (
@@ -34,41 +32,84 @@ CREATE TABLE `etlap` (
   `nev` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `leiras` text COLLATE utf8mb4_hungarian_ci NOT NULL,
   `ar` int(11) NOT NULL,
-  `kategoria` enum('előétel','főétel','desszert') COLLATE utf8mb4_hungarian_ci NOT NULL
+  `kategoria_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `etlap`
+-- Dumping data for table `etlap`
 --
 
-INSERT INTO `etlap` (`id`, `nev`, `leiras`, `ar`, `kategoria`) VALUES
-(1, 'Húsleves', 'Tyúkhússal, Cérnametélttel', 1390, 'előétel'),
-(2, 'Bableves', 'Füstölt csülökkel', 1590, 'előétel'),
-(3, 'Gyros tál', 'Gyroshússal, hasábburgonyával, 5 féle salátával ', 1890, 'főétel'),
-(4, 'Velővel töltött borda', 'Kirántva, hasábburgonyával', 1890, 'főétel'),
-(5, 'Rib-Eye Steak', 'BBQ zöldbabbal', 4950, 'főétel'),
-(6, 'Tiramisu', '', 890, 'desszert'),
-(7, 'Palacsinta', 'Kakaós, Nutellás, Lekváros', 490, 'desszert');
+INSERT INTO `etlap` (`id`, `nev`, `leiras`, `ar`, `kategoria_id`) VALUES
+(1, 'Húsleves', 'Tyúkhússal, Cérnametélttel', 1390, 1),
+(2, 'Bableves', 'Füstölt csülökkel', 1590, 1),
+(3, 'Gyros tál', 'Gyroshússal, hasábburgonyával, 5 féle salátával ', 1890, 2),
+(4, 'Velővel töltött borda', 'Kirántva, hasábburgonyával', 1890, 2),
+(5, 'Rib-Eye Steak', 'BBQ zöldbabbal', 4950, 2),
+(6, 'Tiramisu', '', 890, 3),
+(7, 'Palacsinta', 'Kakaós, Nutellás, Lekváros', 490, 3);
+
+-- --------------------------------------------------------
 
 --
--- Indexek a kiírt táblákhoz
+-- Table structure for table `kategoria`
+--
+
+CREATE TABLE `kategoria` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- Dumping data for table `kategoria`
+--
+
+INSERT INTO `kategoria` (`id`, `nev`) VALUES
+(1, 'előétel'),
+(2, 'főétel'),
+(3, 'desszert');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `etlap`
+-- Indexes for table `etlap`
 --
 ALTER TABLE `etlap`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoria_id` (`kategoria_id`);
+
+--
+-- Indexes for table `kategoria`
+--
+ALTER TABLE `kategoria`
   ADD PRIMARY KEY (`id`);
 
 --
--- A kiírt táblák AUTO_INCREMENT értéke
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT a táblához `etlap`
+-- AUTO_INCREMENT for table `etlap`
 --
 ALTER TABLE `etlap`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `kategoria`
+--
+ALTER TABLE `kategoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `etlap`
+--
+ALTER TABLE `etlap`
+  ADD CONSTRAINT `etlap_ibfk_1` FOREIGN KEY (`kategoria_id`) REFERENCES `kategoria` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
